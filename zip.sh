6822a8f39a0bf4168ff12a22c5da5e58705e9646
Gereon Fox <ger.fox07@t-online.de>
2013-05-01 10:28:11 +0200

    Moved the samples to 'samples' Updated zip.sh to include some ccs samples.

--- zip.sh
+++ zip.sh
@@ -1,1 +1,1 @@
-zip -o ccs_lts.zip -xi ccs.sml ccs_wizard.sh general.sml lts.sml set.sml stepper.sml 
+zip -o ccs_lts.zip -xi ccs.sml ccs_wizard.sh general.sml lts.sml set.sml stepper.sml samples/protocol.ccs samples/camera.ccs
