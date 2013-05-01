f64c86ced332e7801580d344be298f3730a979c6
Gereon Fox <ger.fox07@t-online.de>
2013-05-01 10:45:58 +0200

    Included the dummy convert.sml in the zip files.

--- zip.sh
+++ zip.sh
@@ -1,1 +1,1 @@
-zip -o ccs_lts.zip -xi ccs.sml ccs_wizard.sh general.sml lts.sml set.sml stepper.sml samples/protocol.ccs samples/camera.ccs
+zip -o ccs_lts.zip -xi ccs.sml ccs_wizard.sh convert.sml general.sml lts.sml set.sml stepper.sml samples/protocol.ccs samples/camera.ccs
