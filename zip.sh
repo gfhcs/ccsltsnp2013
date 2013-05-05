09cfbea6390bb74a5ed81f44113bb4aafa8f5773
Gereon Fox <ger.fox07@t-online.de>
2013-05-05 22:25:45 +0200

    Renamed the zip file and properly added the directorys "ccs_wizard" and "graphs".

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
-rm ccs_lts.zip
+rm ccs_wizard.zip
 
 cd ../
 
-zip -o ccs_wizard/ccs_lts.zip -xi ccs_wizard/ccs.sml ccs_wizard/ccs_wizard.sh ccs_wizard/ccs_wizard.cmd ccs_wizard/general.sml ccs_wizard/lts.sml ccs_wizard/set.sml ccs_wizard/stepper.sml ccs_wizard/samples/protocol.ccs ccs_wizard/samples/camera.ccs ccs_wizard/README.txt
+zip -o ccs_wizard/ccs_wizard.zip -xi ccs_wizard ccs_wizard/ccs.sml ccs_wizard/ccs_wizard.sh ccs_wizard/ccs_wizard.cmd ccs_wizard/general.sml ccs_wizard/lts.sml ccs_wizard/set.sml ccs_wizard/stepper.sml ccs_wizard/samples ccs_wizard/samples/camera.ccs ccs_wizard/samples/protocol.ccs ccs_wizard/README.txt ccs_wizard/graphs
